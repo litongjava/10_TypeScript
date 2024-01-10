62e9308afcb0a21a85ab0a9e3efb27d7feb07f8a
litongjava <litongjava@qq.com>
2024-01-09 22:06:23 -1000

    add Dockerfile

--- typescript-base/L02_TypeScriptBase/hello.js
+++
@@ -1,7 +0,0 @@
-/**
-    npm install -g typescript
- */
-function add(a, b) {
-    return a + b;
-}
-console.log(add(10));
--- typescript-base/L02_TypeScriptBase/hello.ts
+++
@@ -1,9 +0,0 @@
-/**
-    npm install -g typescript
- */
-
-function add(a:number,b:number){
-    return a+b;
-}
-
-console.log(add(10,20));
